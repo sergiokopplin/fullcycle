fefcd5a3e5462e111f11264f77047df2d23964cb
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2025-04-26 15:16:16 -0300

    updates files

---
+++ rabbitmq/src/01-connection/connection.ts
@@ -0,0 +1,23 @@
+import * as amqp from 'amqplib';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function connect() {
+    try {
+        const connection = await amqp.connect('amqp://admin:admin@localhost:5672');
+        console.log('Connected to RabbitMQ');
+
+        const channel = await connection.createChannel();
+        console.log('Channel created');
+
+        await sleep(30000);
+
+        await channel.close();
+        await connection.close();
+        console.log('Closed connection and channel');
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+    }
+}
+
+connect();
